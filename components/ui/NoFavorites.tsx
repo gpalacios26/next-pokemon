2096f5776037b36e6cf575d72c28b3a0b3af3b8d
Gregory Palacios <gregpalacios26@gmail.com>
2022-03-30 11:05:16 -0500

    Agregando incremental static generation con next

--- components/ui/NoFavorites.tsx
+++ components/ui/NoFavorites.tsx
@@ -12,7 +12,7 @@ export const NoFavorites = () => {
         }}>
             <Text h1>No hay favoritos</Text>
             <Image src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg'
-                width={250} height={250} css={{ opacity: 0.1 }}></Image>
+                alt="No hay favoritos" width={250} height={250} css={{ opacity: 0.1 }}></Image>
         </Container>
     )
 }
